0f23c769eda879e871c91035183a83c22472b643
Siddharth Mishra <simishra@tesla.com>
2019-09-26 14:27:49 -0600

    - Learn Go in 2 minutes

--- hello.go
+++ hello.go
@@ -16,6 +16,9 @@ func main() {
 	fmt.Println(add(4, 5))
 	fmt.Println(swap("siddharth", "mishra"))
 	fmt.Println(split(13))
+	arrayExample()
+	dictionaryExample()
+	forloopExample()
 }
 
 func add(x, y int) int {
@@ -31,4 +34,57 @@ func split(sum int) (x, y int) {
 	y = sum - x
 
 	return
+}
+
+func arrayExample() {
+	arr := [10]int{1, 2}
+	arr[7] = 34
+	fmt.Println(arr)
+
+	arr01 := []int{1, 2}
+	arr01 = append(arr01, 45)
+	fmt.Println(arr01)
+}
+
+func dictionaryExample() {
+	vertices := make(map[string]int)
+	vertices["triangle"] = 3
+	vertices["square"] = 4
+	vertices["dodecagon"] = 12
+	fmt.Println(vertices)
+	fmt.Println(vertices["triangle"])
+	fmt.Println(vertices["sad"])
+	delete(vertices, "square")
+	fmt.Println(vertices)
+	delete(vertices, "sad")
+	fmt.Println(vertices)
+}
+
+func forloopExample() {
+	for index := 0; index < 5; index++ {
+		fmt.Println(index)
+	}
+
+	fmt.Println("***While Loop")
+	i := 0
+	for i < 6 {
+		fmt.Println(i)
+		i++
+	}
+
+	fmt.Println("**** For loop with Array")
+	arr := []string{"a", "b", "c"}
+
+	for index, value := range arr {
+		fmt.Println("index", index, "value", value)
+	}
+
+	fmt.Println("**** For loop with Array")
+	dictionary := make(map[string]string)
+	dictionary["firstName"] = "siddharth"
+	dictionary["last name"] = "mishra"
+
+	for key, value := range dictionary {
+		fmt.Println("key", key, "value", value)
+	}
 }
